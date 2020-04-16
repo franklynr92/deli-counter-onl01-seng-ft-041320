016ce80daadb38294bb783ba5a544384be1f15b6
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 22:49:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,21 @@
 # Write your code here.
 
-def line(a)
-  if katz_deli.length == 0
-  puts "The line is currently empty."
-  arr[]
-end
-end
+# def line(a)
+#   if katz_deli.length == 0
+#   puts "The line is currently empty."
+#   arr[]
+# end
+# end
 
-def take_a_number(katz_deli, other_deli)
-  other_deli[]
-  other_deli.each do |client|
-  puts "The line is currently: #{"client" + 1}"
-  #for each of clients(elements/strings) in the array that we input empty
-  #adds the person to end of the array
+# def take_a_number(katz_deli, other_deli)
+#   other_deli[]
+#   other_deli.each do |client|
+#   puts "The line is currently: #{"client" + 1}"
+#   #for each of clients(elements/strings) in the array that we input empty
+#   #adds the person to end of the array
   
-end  
-end 
+# end  
+# end 
 
 
 # def now_serving
@@ -24,3 +24,9 @@ end
 #   else # condition = 0
 #   puts "There is nobody waiting to be served!"
 # end
+
+def line(q)
+  if q.size == 0
+    puts "The line is currently empty."
+  else 
+    
