b7e830dc35bc468cf16d6875ea7abe33901eaceb
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 21:47:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def line(array)
+  katz_deliz = [] 
+  if katz_deliz.length == 0
+  puts "The line is currently empty."
+  # katz_deliz += 1
+end
+end
+
+def take_a_number(array, line)
+  clients[]
+  clients.each do |client|
+  puts"The line is currently: #{"client"}"
+  #for each of clients(elements/strings) in the array that we input empty
+  #adds the person to end of the array
+  
+end  
+end 
