0a27837b73924be7a499ca5b4f8483a9feda065d
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 22:49:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,11 @@
 # end
 
 def line(q)
-  if q.size == 0
+  if q.size == 0 
     puts "The line is currently empty."
   else 
-    
+    puts "The line is currently: "
+    q.each_with_index {|name, index|
+    puts "#{index} #{name}"}
+  end
+end
