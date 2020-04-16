8ed873e22e0960881d59e50c772170a2e817a14a
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 21:52:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,8 +9,8 @@ end
 
 def take_a_number(array, line)
   other_deli[]
-  other_deli.each do |client|
-  puts "The line is currently: #{"client"}"
+  other_deli.each do |line|
+  puts "The line is currently: #{"line"}"
   #for each of clients(elements/strings) in the array that we input empty
   #adds the person to end of the array
   
