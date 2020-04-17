42a73e643ee8a98a6438eabd594c2de3469c4415
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-17 00:03:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -33,9 +33,8 @@ def now_serving(q)
 if q.size == 0
      puts "There is nobody waiting to be served!" 
   else 
-    q.shift(name)
-    
-    puts "currently serving #{name}"
+    puts "currently serving #{q.first}"
+    q.shift
 # if #condition 
 #   puts "Welcome, #{name}. You are #{number} in line."
 #   else # condition = 0
